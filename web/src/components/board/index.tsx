4930768fad6e2e7c675c627e6592191d81037938
Kent Huang <k@kenthuang.com>
2021-05-26 01:23:01 -0500

    feat(web): init board selector
    + add react-select package
    + board component
    + mock board data

---
+++ web/src/components/board/index.tsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components'
+import Select from 'react-select';
+
+//I'm following the established code layout but I would suggest grouping the type with the component
+// a types.ts in this /src/component/board folder... 
+import {Board} from '../../types/board'
+
+const BoardContainer = styled.div`
+  background-color: rgb(49, 121, 186);
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  color: #393939;
+  overflow-y: hidden;
+  overflow-x: auto;
+  position: absolute;
+  padding: 5px;
+  align-items: flex-start;
+`
+
+const StyledBoardHeader = styled.div`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  background-color: rgb(49, 121, 186);
+`;
+
+const LeftQueue = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin: 10px 10px;
+`;
+
+const BoardSelector = styled.div`
+  width: 280px;
+  border-radius: 3px;
+  color: black;
+  padding: 10px;
+`;
+
+export const BoardHeader = ({currentBoard, boards}: any) => {
+    console.log(currentBoard)
+    const options = boards.map((element: { id: number; title: string; }) => ({
+        "value": element.id, "label": element.title
+    }));
+    return (
+        <StyledBoardHeader>
+            <LeftQueue>
+                <BoardSelector>
+                    <Select
+                        value={options[0]}
+                        label="Board"
+                        options={options}
+                        isSearchable={false}
+                    />
+                </BoardSelector>
+            </LeftQueue>
+        </StyledBoardHeader>
+    )
+}
