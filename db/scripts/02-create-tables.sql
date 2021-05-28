0875285e42595130d59da61175ca4cf00e4e3ecf
Kent Huang <k@kenthuang.com>
2021-05-28 01:30:23 -0500

    chore: modify db scripts to insert data instead of migration

--- db/scripts/02-create-tables.sql
+++ db/scripts/02-create-tables.sql
@@ -6,5 +6,13 @@ CREATE TABLE sections (
 CREATE TABLE cards (
     id serial PRIMARY KEY,
     title text NOT NULL,
-    section_id serial
+    section_id serial,
+    board_id serial
 );
+
+-- I decide not put the unique constraint on table name.
+-- If we were to add the concept of teams, the boards would be unique to a team/organization
+CREATE TABLE boards (
+    id serial PRIMARY KEY,
+    title text NOT NULL
+)
--- db/scripts/03-create-data.sql
+++ db/scripts/03-create-data.sql
@@ -1,3 +1,6 @@
+-- Insert boards
+INSERT INTO board (id, title) VALUES (1, "Trello");
+
 -- Insert sections
 INSERT INTO sections(title) VALUES ('Backlog');
 INSERT INTO sections(title) VALUES ('Ready for Development');
@@ -6,6 +9,6 @@ INSERT INTO sections(title) VALUES ('In Review');
 INSERT INTO sections(title) VALUES ('Done');
 
 -- Insert cards
-INSERT INTO cards(title, section_id) VALUES ('Card drag/drop', (SELECT id FROM sections WHERE title='Backlog'));
-INSERT INTO cards(title, section_id) VALUES ('Card details', (SELECT id FROM sections WHERE title='Backlog'));
-INSERT INTO cards(title, section_id) VALUES ('Multiple boards', (SELECT id FROM sections WHERE title='Backlog'));
+INSERT INTO cards(title, section_id, board_id) VALUES ('Card drag/drop', (SELECT id FROM sections WHERE title='Backlog'), 1);
+INSERT INTO cards(title, section_id, board_id) VALUES ('Card details', (SELECT id FROM sections WHERE title='Backlog'), 1);
+INSERT INTO cards(title, section_id, board_id) VALUES ('Multiple boards', (SELECT id FROM sections WHERE title='Backlog'), 1);
