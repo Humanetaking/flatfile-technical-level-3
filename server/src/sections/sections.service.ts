37aebf373fb5601d31f54896b1f14227ab9f0816
Kent Huang <k@kenthuang.com>
2021-05-28 16:09:11 -0500

    fix: Display sections without cards too

--- server/src/sections/sections.service.ts
+++ server/src/sections/sections.service.ts
@@ -10,14 +10,30 @@ export class SectionsService {
     private sectionsRepository: Repository<SectionEntity>
   ) {}
 
-  findAll(boardId: number): Promise<SectionEntity[]> {
-    return this.sectionsRepository.find(
+  async findAll(boardId: number): Promise<SectionEntity[]> {
+
+    // I spent an hour on trying to figure typeorm's innerjoin and decided to make another call to db and merge it
+    let sections = await this.sectionsRepository.find();
+
+        
+    let sectionsWithCards = await this.sectionsRepository.find(
       {
         relations: ['cards'],
-        where: condition =>{
-          condition.where('SectionEntity__cards.board_id = :boardId', { boardId: boardId})
+        where: condition => {
+          condition.where('SectionEntity__cards.board_id = :boardId', { boardId: boardId });
         }
       }
     )
+    
+    sections.forEach(section => {
+      section["cards"] = []
+      sectionsWithCards.every(sectionWithCards => {
+        if (sectionWithCards.id == section.id) {
+          section["cards"] = sectionWithCards.cards
+        }
+      })
+    });
+
+    return sections
   }
 }
