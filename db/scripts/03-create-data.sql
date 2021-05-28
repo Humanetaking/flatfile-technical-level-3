7110d8e9cfba92880969f066f6a178d27574b9d1
Kent Huang <k@kenthuang.com>
2021-05-28 01:55:04 -0500

    fix: cards board_id inserts

--- db/scripts/03-create-data.sql
+++ db/scripts/03-create-data.sql
@@ -1,5 +1,5 @@
 -- Insert boards
-INSERT INTO board (id, title) VALUES (1, "Trello");
+INSERT INTO boards(title) VALUES ('Trello');
 
 -- Insert sections
 INSERT INTO sections(title) VALUES ('Backlog');
@@ -9,6 +9,6 @@ INSERT INTO sections(title) VALUES ('In Review');
 INSERT INTO sections(title) VALUES ('Done');
 
 -- Insert cards
-INSERT INTO cards(title, section_id, board_id) VALUES ('Card drag/drop', (SELECT id FROM sections WHERE title='Backlog'), 1);
-INSERT INTO cards(title, section_id, board_id) VALUES ('Card details', (SELECT id FROM sections WHERE title='Backlog'), 1);
-INSERT INTO cards(title, section_id, board_id) VALUES ('Multiple boards', (SELECT id FROM sections WHERE title='Backlog'), 1);
+INSERT INTO cards(title, section_id, board_id) VALUES ('Card drag/drop', (SELECT id FROM sections WHERE title='Backlog'), (SELECT id FROM boards WHERE title='Trello'));
+INSERT INTO cards(title, section_id, board_id) VALUES ('Card details', (SELECT id FROM sections WHERE title='Backlog'), (SELECT id FROM boards WHERE title='Trello'));
+INSERT INTO cards(title, section_id, board_id) VALUES ('Multiple boards', (SELECT id FROM sections WHERE title='Backlog'), (SELECT id FROM boards WHERE title='Trello'));
