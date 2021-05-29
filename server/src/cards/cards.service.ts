42656a534d6da3940ff909139b74a7e622384c79
Kent Huang <k@kenthuang.com>
2021-05-28 20:20:54 -0500

    feat: card require boardId argument

--- server/src/cards/cards.service.ts
+++ server/src/cards/cards.service.ts
@@ -10,10 +10,11 @@ export class CardsService {
     private cardsRepository: Repository<CardEntity>
   ) {}
 
-  create({ sectionId, title }: { sectionId: number; title: string }): Promise<CardEntity> {
+  create({ boardId, sectionId, title }: {boardId:number, sectionId: number; title: string }): Promise<CardEntity> {
     let card = new CardEntity()
     card.title = title
     card.section_id = sectionId
+    card.board_id = boardId
     return this.cardsRepository.save(card)
   }
 }
